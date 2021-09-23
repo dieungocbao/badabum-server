24076f3a432998afdc9b402ced38deb58d0f437a
dieungocbao <dieungocbao@gmail.com>
2021-09-23 22:38:26 +0700

    search controller

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,8 +3,9 @@ import PostsController from './posts.controller'
 import PostsService from './posts.service'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Post } from './post.entity'
+import { SearchModule } from '../search/search.module'
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), SearchModule],
   controllers: [PostsController],
   providers: [PostsService],
 })
